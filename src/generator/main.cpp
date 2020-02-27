cecf683f740917a559c7bbbf685818ba75661aec
JoseFilipeFerreira <36403066+JoseFilipeFerreira@users.noreply.github.com>
2020-02-27 15:05:17 +0000

    add inital structure (#1)

---
+++ src/generator/main.cpp
@@ -0,0 +1,15 @@
+#include <GL/gl.h>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+int main(int argc, char **argv) {
+
+	return 1;
+}
+

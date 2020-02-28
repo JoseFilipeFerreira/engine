e389135ab8247556eabd4db3b6eceeaf0d4a2ee7
jtexeira <38398296+jtexeira@users.noreply.github.com>
2020-02-28 10:08:23 +0000

    Plane Main (#3)

--- src/generator/main.cpp
+++ src/generator/main.cpp
@@ -1,15 +1,19 @@
-#include <GL/gl.h>
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
-#include <GL/glut.h>
-#endif
+#include<string>
+#include "./plane.hpp"
+#include<ostream>
+#include<iostream>
+using namespace std;
 
-#define _USE_MATH_DEFINES
-#include <math.h>
-
-int main(int argc, char **argv) {
-
-	return 1;
+int main(int argc, char** argv) {
+    string type(argv[1]);
+    Plane* p;
+    if("plane" == type) {
+        p = new Plane(argc - 2, argv + 2);
+        for(Point l: p->draw()) {
+            cout << l.to_string() << endl ;
+        }
+    }
+    else if("box" == type) {}
+    else if("sphere" == type) {}
+    else if("cone" == type) {}
 }
-

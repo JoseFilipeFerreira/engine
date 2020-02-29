af6df5cdc1476534e1b265e8d27fc2783606a75a
MightyMime <jose.filipe.matos.ferreira@gmail.com>
2020-02-29 13:17:50 +0000

    [engine] add cmakelist

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -1,9 +1,21 @@
+#include <GL/gl.h>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include "model.hpp"
+
 #include "../deps/rapidxml.hpp"
 #include <fstream>
-#include <iostream>
 #include <string.h>
 #include <string>
 #include <vector>
+#include <math.h>
+#include<string>
+#include<ostream>
+#include<iostream>
 
 using namespace std;
 using namespace rapidxml;
@@ -22,4 +34,91 @@ vector<string> read_models() {
     for(xml_node<> *n = doc.first_node()->first_node(); n; n = n->next_sibling()) 
         res.push_back(n->first_attribute()->value());
     return res;
+
+void changeSize(int w, int h) {
+
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window with zero width).
+	if(h == 0)
+		h = 1;
+
+	// compute window's aspect ratio 
+	float ratio = w * 1.0 / h;
+
+	// Set the projection matrix as current
+	glMatrixMode(GL_PROJECTION);
+	// Load Identity Matrix
+	glLoadIdentity();
+	
+	// Set the viewport to be the entire window
+        glViewport(0, 0, w, h);
+
+	// Set perspective
+	gluPerspective(45.0f ,ratio, 1.0f ,1000.0f);
+
+	// return to the model view matrix mode
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void draw_axis(){
+    // axis
+    glBegin(GL_LINES);
+        // X axis in red
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex3f(-100.0f, 0.0f, 0.0f);
+        glVertex3f( 100.0f, 0.0f, 0.0f);
+        // Y Axis in Green
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex3f(0.0f, -100.0f, 0.0f);
+        glVertex3f(0.0f, 100.0f, 0.0f);
+        // Z Axis in Blue
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex3f(0.0f, 0.0f, -100.0f);
+        glVertex3f(0.0f, 0.0f,  100.0f);
+    glEnd();
+}
+
+void renderScene(void) {
+
+	// clear buffers
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// set the camera
+    glLoadIdentity();
+    gluLookAt(5.0,5.0,5.0, 
+		      0.0,0.0,0.0,
+			  0.0f,1.0f,0.0f);
+
+    draw_axis();
+
+    Model("plane.3d").draw_model();
+
+    // End of frame
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv) {
+
+    // init GLUT and the window
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(800,800);
+    glutCreateWindow("CG@DI-UM");
+		
+    // Required callback registry 
+    glutDisplayFunc(renderScene);
+    glutReshapeFunc(changeSize);
+
+    // put here the registration of the keyboard callbacks
+//    glutKeyboardFunc(reactKey);
+
+    //  OpenGL settings
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+	
+    // enter GLUT's main cycle
+    glutMainLoop();
+	
+    return 1;
 }

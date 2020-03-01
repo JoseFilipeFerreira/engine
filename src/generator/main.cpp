a084a890917523ea197de9c09be0fb8acbe49f97
jtexeira <38398296+jtexeira@users.noreply.github.com>
2020-03-01 12:40:45 +0000

    Templates are now a thing (#7)

--- src/generator/main.cpp
+++ src/generator/main.cpp
@@ -5,20 +5,19 @@
 #include<fstream>
 using namespace std;
 
-void file_writer(vector<Point> v, string filename) {
+template<typename T>
+void file_writer(T v, string filename) {
     ofstream f;
     f.open(filename);
-    for(Point p: v)
+    for(Point p: v.draw())
         f << p.to_string() << endl;
 }
 
 int main(int argc, char** argv) {
     string type(argv[1]);
-    Primitive* p;
     if("plane" == type)
-        p = new Plane(argc - 3, argv + 2);
+        file_writer(Plane(argc - 3, argv + 2), argv[argc-1]);
     else if("box" == type) {}
     else if("sphere" == type) {}
     else if("cone" == type) {}
-    file_writer(p->draw(), argv[argc-1]);
 }

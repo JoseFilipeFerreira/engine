00c54130f53a3b6c862d014b2bc392ae34d4edb1
MightyMime <jose.filipe.matos.ferreira@gmail.com>
2020-03-06 23:16:56 +0000

    add help message

--- src/generator/main.cpp
+++ src/generator/main.cpp
@@ -11,6 +11,15 @@
 #include<iostream>
 #include<fstream>
 
+static std::string help_String(
+"Usage: generator [PRIMITIVA] [ARGS] [FILE NAME]\n\n"
+"PRIMITIVA  ARGS\n"
+"plane      [side]\n"
+"box        [x] [y] [z] [divisions]\n"
+"sphere     [radius] [slices] [stacks]\n"
+"cone       [radius] [height] [slices] [stacks]\n"
+"cylinder   [radius] [height] [slices] [stacks]");
+
 template<typename T>
 void file_writer(T v, std::string filename) {
     std::ofstream f;
@@ -31,4 +40,6 @@ int main(int argc, char** argv) {
         file_writer(Cone(argc - 3, argv + 2), argv[argc-1]);
     else if("cylinder" == type)
         file_writer(Cylinder(argc - 3, argv + 2), argv[argc-1]);
+    else
+        std::cout << help_String << std::endl;
 }

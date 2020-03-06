b7c8bf40d53a1adefd20f21d25ed07cb6f05111f
JoseFilipeFerreira <36403066+JoseFilipeFerreira@users.noreply.github.com>
2020-03-06 23:27:48 +0000

    [report] add report (#13)
    * [report] add report
    
    * forgot this (#14)
    
    * add true randomness
    
    * [generator] improve cilinder drawing
    
    * [report] add report
    
    * add more
    
    * ahhhh
    
    * typos!
    
    * pls help
    
    * add models and scenes
    
    * improve cone
    
    * add scenes
    
    * no more flicker
    
    * add help message

--- src/generator/cone.cpp
+++ src/generator/cone.cpp
@@ -17,24 +17,23 @@ std::vector<Point> Cone::draw() const {
     std::vector<Point> coords;
 
     float ang_slice = 2 * M_PI / _slices;
-
     Point top_point = Point(0, _height, 0);
-
-    for(i32 slice = 0; slice < _slices; slice++) {
-        coords.push_back(PointSpherical(_radius, M_PI/2, ang_slice *(slice+1)));
-        coords.push_back(PointSpherical(_radius, M_PI/2, ang_slice * slice));
-        coords.push_back(PointSpherical(0, 0, 0));
-    }
 
     for(i32 slice = 0; slice < _slices; slice++) {
         Point base   = PointSpherical(_radius, M_PI/2, ang_slice *  slice);
         Point n_base = PointSpherical(_radius, M_PI/2, ang_slice * (slice+1));
 
-        Vector step = Vector(base, top_point) / _stacks;
+        //base
+        coords.push_back(n_base);
+        coords.push_back(base);
+        coords.push_back(Point(0, 0, 0));
+
+        Vector step   = Vector(  base, top_point) / _stacks;
         Vector n_step = Vector(n_base, top_point) / _stacks;
 
+        //side
         for(i32 stack = 0; stack < _stacks; stack++) {
-            Point pivot =  base + step * stack;
+            Point pivot   =    base +   step * stack;
             Point n_pivot =  n_base + n_step * stack;
 
             if(!(stack == _stacks -1)) {

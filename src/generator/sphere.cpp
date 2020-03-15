4de6de1a60952965134ae9a154679b996db3b912
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2020-03-15 22:21:52 +0000

    fix sphere drawing

--- src/generator/sphere.cpp
+++ src/generator/sphere.cpp
@@ -18,8 +18,8 @@ std::vector<Point> Sphere::draw() const {
     float ang_stack = M_PI / _stacks;
 
     for(i32 slice = 0; slice < _slices; slice++)
-        for(i32 stack = 0; stack < _stacks; stack++) {
-            if(! (stack == 0 || stack == _stacks-1)) {
+        for(i32 stack = 0; stack < _stacks + 1; stack++) {
+            if((stack != 0) && (stack != _stacks)) {
                 //1st triangle
                 coords.push_back(PointSpherical(_radius, ang_stack * (stack+1), ang_slice * (slice+1)));
                 coords.push_back(PointSpherical(_radius, ang_stack * stack    , ang_slice * (slice+1)));

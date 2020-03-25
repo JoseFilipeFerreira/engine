894ac696f0fad0126414fe3d4a7b77e368ee7909
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2020-03-25 16:27:11 +0000

    [scripts] improve string formating

--- scripts/solar_system.py
+++ scripts/solar_system.py
@@ -12,31 +12,29 @@ SCALE = 3
 
 def astro_radius(name):
     with open('planets.csv','rt')as f:
-        data = csv.reader(f)
-        next(data)
+        data = csv.DictReader(f)
         for row in data:
-            if(row[0] == name):
-                return float(row[1])/60000
+            if(row["planet"] == name):
+                return float(row["radius"])/60000
 
     with open('satellites.csv','rt')as f:
-        data = csv.reader(f)
-        next(data)
+        data = csv.DictReader(f)
         for row in data:
-            if(row[1] == name):
-                size = float(row[3])
+            if(row["name"] == name):
+                size = float(row["radius"])
                 scale = size / 60000
                 if size < 1000:
                     scale *= (0.01/scale)
                 return scale
 
-def generate_moons(planet_name, min_dist, max_dist):
+def generate_moons(planet_name, planet_radius, colour):
     moons = []
     with open('satellites.csv','rt')as f:
-        data = csv.reader(f)
+        data = csv.DictReader(f)
         for row in data:
-            if(row[0] == planet_name):
-                dist = random.uniform(min_dist, max_dist)
-                moons.append(Astro(row[1], dist, "#FF0000"))
+            if(row["planet"] == planet_name):
+                dist = random.uniform(planet_radius*1.5, planet_radius*2.5)
+                moons.append(Astro(row["name"], dist, colour))
     return moons
 
 class Astro:
@@ -48,14 +46,17 @@ class Astro:
             self.radius = radius
         self.distance = distance
         self.colour = colour
-        self.moons = generate_moons(name, self.radius*1.5, self.radius*2.5)
+        self.moons = generate_moons(name, self.radius, "#808080")
 
     def print_astro(self, curr_radius=SCALE, indent=7):
-        print(' ' * indent, '<!-- {} -->'.format(self.name))
-        print(' ' * indent, '<group colour="{0}">'.format(self.colour))
-        print(' ' * indent, '    <rotate axisX="0" axisY="1" axisZ="0" angle="{0}"/>'.format(random.uniform(0,360)))
-        print(' ' * indent, '    <translate X="0" Y="0" Z="{0}" />'.format(self.distance / curr_radius))
-        print(' ' * indent, '    <scale X="{0}" Y="{0}" Z="{0}" />'.format(self.radius / curr_radius))
+        rng_r = random.uniform(0,360)
+        curr_translate = self.distance / curr_radius
+        curr_scale = self.radius / curr_radius
+        print(' ' * indent, f'<!-- {self.name} -->')
+        print(' ' * indent, f'<group colour="{self.colour}">')
+        print(' ' * indent, f'    <rotate axisX="0" axisY="1" axisZ="0" angle="{rng_r}"/>')
+        print(' ' * indent, f'    <translate X="0" Y="0" Z="{curr_translate}" />')
+        print(' ' * indent, f'    <scale X="{curr_scale}" Y="{curr_scale}" Z="{curr_scale}" />')
         print(' ' * indent, '    <models>')
         print(' ' * indent, '        <model file="models/sphere.3d"/>')
         print(' ' * indent, '    </models>')
@@ -74,10 +75,9 @@ def draw_asteroid_belt(name, number, min_dist, max_dist, size, colour):
 
 def get_planets():
     with open('planets.csv','rt')as f:
-        data = csv.reader(f)
-        next(data)
+        data = csv.DictReader(f)
         for row in data:
-            yield Astro(row[0], SCALE + float(row[2]), row[3])
+            yield Astro(row["planet"], SCALE + float(row["relative distance"]), row["color"])
 
 print('<scene>')
 print('    <!--Sun-->')
@@ -89,8 +89,8 @@ print('        </models>')
 for p in get_planets():
     p.print_astro()
 
-draw_asteroid_belt("Asteroid belt", 200 , SCALE + 8  , SCALE + 9  , 0.02, "#FF00AA")
-draw_asteroid_belt("Kuiper Belt"  , 1000, SCALE + 100, SCALE + 103, 0.05, "#FF00AA")
+draw_asteroid_belt("Asteroid belt", 200 , SCALE + 8  , SCALE + 9  , 0.02, "#808080")
+draw_asteroid_belt("Kuiper Belt"  , 1000, SCALE + 100, SCALE + 103, 0.05, "#808080")
 
 print('    </group>')
 print('</scene>')

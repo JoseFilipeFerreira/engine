ec982affcb1a6297c93f2a94431d54c1498ee3ec
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2020-03-25 18:19:16 +0000

    not eorking sad

--- src/generator/torus.cpp
+++ src/generator/torus.cpp
@@ -16,15 +16,17 @@ std::vector<Point> Torus::draw() const {
     std::vector<Point> coords;
 
     float ang_slice = 2 * M_PI / _slices;
-    float ang_stack = M_PI / _stacks;
+    float ang_stack = 2 * M_PI / _stacks;
 
     for(i32 slice = 0; slice < _slices; slice++) {
         PointSpherical   center = PointSpherical(_radius, M_PI/2, ang_slice * slice);
         PointSpherical n_center = PointSpherical(_radius, M_PI/2, ang_slice * (slice+1));
         for(i32 stack = 0; stack < _stacks * 2; stack++)
         {
-            VectorSpherical   border = VectorSpherical(_ring_radius, ang_stack * slice      ,   center.azimuth());
-            VectorSpherical n_border = VectorSpherical(_ring_radius, ang_stack * (slice + 1), n_center.azimuth() );
+            //VectorSpherical   border = VectorSpherical(_ring_radius, ang_stack * stack      ,   center.azimuth());
+            //VectorSpherical n_border = VectorSpherical(_ring_radius, ang_stack * (stack + 1), n_center.azimuth() );
+            auto border = Vector(0, 1, 0);
+            auto n_border = Vector(0, 1, 0);
 
             coords.push_back(  center +   border);
             coords.push_back(n_center +   border);

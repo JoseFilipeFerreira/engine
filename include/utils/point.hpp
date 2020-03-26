40f5236362c2d36b13ee99d5191160c9d0ca4821
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2020-03-26 14:55:28 +0000

    [utils] const everywhere

--- include/utils/point.hpp
+++ include/utils/point.hpp
@@ -1,5 +1,6 @@
 #ifndef POINT_H
 #define POINT_H
+#include <cmath>
 #include <string>
 
 class Point;
@@ -19,14 +20,18 @@ public:
   auto constexpr x() const noexcept -> float { return _x; }
   auto constexpr y() const noexcept -> float { return _y; }
   auto constexpr z() const noexcept -> float { return _z; }
-  Vector unit() const;
-  Vector hadamard(Vector) const;
-  Vector hadamard(float, float, float) const;
-  Vector operator+(Vector v) const {
+  auto unit() const -> Vector;
+  auto hadamard(Vector) const -> Vector;
+  auto hadamard(float, float, float) const -> Vector;
+  auto operator+(Vector v) const -> Vector {
     return Vector(_x + v.x(), _y + v.y(), _z + v.z());
   }
-  Vector operator*(int s) const { return Vector(_x * s, _y * s, _z * s); }
-  Vector operator/(int s) const { return Vector(_x / s, _y / s, _z / s); }
+  auto operator*(int s) const -> Vector {
+    return Vector(_x * s, _y * s, _z * s);
+  }
+  auto operator/(int s) const -> Vector {
+    return Vector(_x / s, _y / s, _z / s);
+  }
 };
 
 class VectorSpherical {
@@ -40,7 +45,10 @@ public:
   auto constexpr radius() const noexcept -> float { return _radius; }
   auto constexpr inclination() const noexcept -> float { return _inclination; }
   auto constexpr azimuth() const noexcept -> float { return _azimuth; }
-  VectorSpherical operator+(VectorSpherical const &v) {
+  auto radius(float) const -> VectorSpherical;
+  auto inclination(float) const -> VectorSpherical;
+  auto azimuth(float) const -> VectorSpherical;
+  auto operator+(VectorSpherical const &v) const -> VectorSpherical {
     return VectorSpherical(Vector(*this) + Vector(v));
   }
 };
@@ -56,13 +64,13 @@ public:
   auto constexpr x() const noexcept -> float { return _x; }
   auto constexpr y() const noexcept -> float { return _y; }
   auto constexpr z() const noexcept -> float { return _z; }
-  void x(float x) { _x += x; }
-  void y(float x) { _y += x; }
-  void z(float x) { _z += x; }
-  Point operator+(Vector const &p) {
+  auto x(float) const -> Point;
+  auto y(float) const -> Point;
+  auto z(float) const -> Point;
+  auto operator+(Vector const &p) const -> Point {
     return Point(_x + p.x(), _y + p.y(), _z + p.z());
   }
-  Point operator+(VectorSpherical const &vs) {
+  auto operator+(VectorSpherical const &vs) const -> Point {
     Vector v = Vector(vs);
     return Point(_x + v.x(), _y + v.y(), _z + v.z());
   }
@@ -79,13 +87,13 @@ public:
   auto constexpr radius() const noexcept -> float { return _radius; }
   auto constexpr inclination() const noexcept -> float { return _inclination; }
   auto constexpr azimuth() const noexcept -> float { return _azimuth; }
-  void radius(float x) { _radius += x; }
-  void inclination(float x) { _inclination += x; }
-  void azimuth(float x) { _azimuth += x; }
-  PointSpherical operator+(Vector const &v) {
+  auto radius(float) const -> PointSpherical;
+  auto inclination(float) const -> PointSpherical;
+  auto azimuth(float) const -> PointSpherical;
+  auto operator+(Vector const &v) const -> PointSpherical {
     return PointSpherical(Point(*this) + v);
   }
-  PointSpherical operator+(VectorSpherical const &v) {
+  auto operator+(VectorSpherical const &v) const -> PointSpherical {
     return PointSpherical(Point(*this) + Vector(v));
   }
 };
--- src/utils/point.cpp
+++ src/utils/point.cpp
@@ -3,83 +3,117 @@
 #include <cmath>
 #include <sstream>
 
-Vector::Vector(float x, float y, float z): _x(x), _y(y), _z(z) {}
+float normalize_angle(float a) {
+  if (a < -M_PI)
+    return normalize_angle(a + 2 * M_PI);
+  if (a > M_PI)
+    return normalize_angle(a - 2 * M_PI);
+  return a;
+}
+
+Vector::Vector(float x, float y, float z) : _x(x), _y(y), _z(z) {}
 
 Vector::Vector(Point p1, Point p2) {
-    _x = p2.x() - p1.x();
-    _y = p2.y() - p1.y();
-    _z = p2.z() - p1.z();
+  _x = p2.x() - p1.x();
+  _y = p2.y() - p1.y();
+  _z = p2.z() - p1.z();
 }
 
 Vector::Vector(VectorSpherical p) {
-    _x = p.radius() * sin(p.inclination()) * sin(p.azimuth());
-    _y = p.radius() * cos(p.inclination());
-    _z = p.radius() * sin(p.inclination()) * cos(p.azimuth());
+  _x = p.radius() * sin(p.inclination()) * sin(p.azimuth());
+  _y = p.radius() * cos(p.inclination());
+  _z = p.radius() * sin(p.inclination()) * cos(p.azimuth());
 }
 
-Vector Vector::unit() const {
-    float d = std::sqrt(_x * _x + _y * _y + _z * _z);
-    return Vector(_x / d, _y / d, _z / d);
+auto Vector::unit() const -> Vector {
+  float d = std::sqrt(_x * _x + _y * _y + _z * _z);
+  return Vector(_x / d, _y / d, _z / d);
 }
 
-Vector Vector::hadamard(Vector v) const {
-    return Vector(_x * v.x(), _y * v.y(), _z * v.z());
+auto Vector::hadamard(Vector v) const -> Vector {
+  return Vector(_x * v.x(), _y * v.y(), _z * v.z());
 }
 
-Vector Vector::hadamard(float x, float y, float z) const {
-    return Vector(_x * x, _y * y, _z * z);
+auto Vector::hadamard(float x, float y, float z) const -> Vector {
+  return Vector(_x * x, _y * y, _z * z);
+}
+
+auto VectorSpherical::radius(float x) const -> VectorSpherical {
+  return VectorSpherical(_radius + x, _inclination, _azimuth);
+}
+
+auto VectorSpherical::inclination(float x) const -> VectorSpherical {
+  return VectorSpherical(_radius, normalize_angle(_inclination + x), _azimuth);
+}
+auto VectorSpherical::azimuth(float x) const -> VectorSpherical {
+  return VectorSpherical(_radius, _inclination, normalize_angle(_azimuth + x));
 }
 
 VectorSpherical::VectorSpherical(float radius, float inclination, float azimuth)
     : _radius(radius), _inclination(inclination), _azimuth(azimuth) {}
 
 VectorSpherical::VectorSpherical(Vector p) {
-    _radius = sqrt(p.x() * p.x() + p.y() * p.y() + p.z() * p.z());
+  _radius = sqrt(p.x() * p.x() + p.y() * p.y() + p.z() * p.z());
 
-    if (_radius == 0)
-        _inclination = 0;
-    else
-        _inclination = acos(p.y() / _radius);
+  if (_radius == 0)
+    _inclination = 0;
+  else
+    _inclination = acos(p.y() / _radius);
 
-    _azimuth = atan2(p.x(), p.z());
+  _azimuth = atan2(p.x(), p.z());
 }
 
 std::string VectorSpherical::to_string() const {
-    std::ostringstream sstream;
-    sstream << _radius << ' ' << _inclination << ' ' << _azimuth;
-    return sstream.str();
+  std::ostringstream sstream;
+  sstream << _radius << ' ' << _inclination << ' ' << _azimuth;
+  return sstream.str();
 }
 
-Point::Point(float x, float y, float z): _x(x), _y(y), _z(z) {}
+Point::Point(float x, float y, float z) : _x(x), _y(y), _z(z) {}
+
+auto Point::x(float v) const -> Point { return Point(_x + v, _y, _z); }
+auto Point::y(float v) const -> Point { return Point(_x, _y + v, _z); }
+auto Point::z(float v) const -> Point { return Point(_x, _y, _z + v); }
 
 PointSpherical::PointSpherical(float radius, float inclination, float azimuth)
     : _radius(radius), _inclination(inclination), _azimuth(azimuth) {}
 
 PointSpherical::PointSpherical(Point p) {
-    _radius = sqrt(p.x() * p.x() + p.y() * p.y() + p.z() * p.z());
+  _radius = sqrt(p.x() * p.x() + p.y() * p.y() + p.z() * p.z());
 
-    if (_radius == 0)
-        _inclination = 0;
-    else
-        _inclination = acos(p.y() / _radius);
+  if (_radius == 0)
+    _inclination = 0;
+  else
+    _inclination = acos(p.y() / _radius);
 
-    _azimuth = atan2(p.x(), p.z());
+  _azimuth = atan2(p.x(), p.z());
 }
 
 Point::Point(PointSpherical p) {
-    _x = p.radius() * sin(p.inclination()) * sin(p.azimuth());
-    _y = p.radius() * cos(p.inclination());
-    _z = p.radius() * sin(p.inclination()) * cos(p.azimuth());
+  _x = p.radius() * sin(p.inclination()) * sin(p.azimuth());
+  _y = p.radius() * cos(p.inclination());
+  _z = p.radius() * sin(p.inclination()) * cos(p.azimuth());
+}
+
+auto PointSpherical::radius(float x) const -> PointSpherical {
+  return PointSpherical(_radius + x, _inclination, _azimuth);
+}
+
+auto PointSpherical::inclination(float x) const -> PointSpherical {
+  return PointSpherical(_radius, normalize_angle(_inclination + x), _azimuth);
+}
+auto PointSpherical::azimuth(float x) const -> PointSpherical {
+  return PointSpherical(_radius, _inclination, normalize_angle(_azimuth + x));
 }
 
 std::string PointSpherical::to_string() const {
-    std::ostringstream sstream;
-    sstream << _radius << ' ' << _inclination << ' ' << _azimuth;
-    return sstream.str();
+  std::ostringstream sstream;
+  sstream << _radius << ' ' << _inclination << ' ' << _azimuth;
+  return sstream.str();
 }
 
 std::string Point::to_string() const {
-    std::ostringstream sstream;
-    sstream << _x << ' ' << _y << ' ' << _z;
-    return sstream.str();
+  std::ostringstream sstream;
+  sstream << _x << ' ' << _y << ' ' << _z;
+  return sstream.str();
 }

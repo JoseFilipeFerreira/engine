92e1a1114f864353cfa50984dc76f3991c531ec0
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2020-05-03 15:36:01 +0100

    improve patches documentation

--- src/generator/patches.cpp
+++ src/generator/patches.cpp
@@ -41,22 +41,28 @@ Patches::Patches(int argc, char** argv) {
 auto bezier_point(
     float t, Point const p0, Point const p1, Point const p2, Point const p3)
     -> Point {
+    // bezier matrix
     const float m[4][4] = {
-        {-1, +3, -3, +1}, {+3, -6, +3, +0}, {-3, +3, +0, +0}, {+1, +0, +0, +0}};
+        {-1.0f, +3.0f, -3.0f, +1.0f},
+        {+3.0f, -6.0f, +3.0f, +0.0f},
+        {-3.0f, +3.0f, +0.0f, +0.0f},
+        {+1.0f, +0.0f, +0.0f, +0.0f}};
 
+    // point matrix
     const float pm[4][3] = {
         {p0.x(), p0.y(), p0.z()},
         {p1.x(), p1.y(), p1.z()},
         {p2.x(), p2.y(), p2.z()},
         {p3.x(), p3.y(), p3.z()}};
 
+    // a = m X pm
     float a[4][3] = {0};
     for (i32 i = 0; i < 4; i++)
         for (i32 j = 0; j < 3; j++)
             for (i32 k = 0; k < 4; k++) a[i][j] += m[i][k] * pm[k][j];
 
+    // pv = tv X a
     const float tv[4] = {t * t * t, t * t, t, 1};
-
     float pv[3] = {0};
     for (i32 j = 0; j < 3; j++)
         for (i32 k = 0; k < 4; k++) pv[j] += tv[k] * a[k][j];

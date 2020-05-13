0d1cb8c3ee76aed248790e5a29f7af2419ea20c8
JoseFilipeFerreira <36403066+JoseFilipeFerreira@users.noreply.github.com>
2020-05-13 16:20:29 +0100

    [generator] add normals and textures to plane (#45)

--- src/generator/plane.cpp
+++ src/generator/plane.cpp
@@ -1,14 +1,14 @@
 #include "generator/plane.hpp"
 
-#include <stdexcept>
-
 Plane::Plane(int argc, char** args) {
     side_length = std::stof(args[0]);
 }
 
-std::vector<Point> Plane::draw() const {
-    std::vector<Point> coords;
+std::vector<ModelPoint> Plane::draw() const {
+    std::vector<ModelPoint> coords;
     float half = side_length / 2;
+
+    auto v = Vector(0, 1, 0);
 
     auto p0 = Point(-half, 0, -half);
     auto p1 = Point( half, 0, -half);
@@ -16,14 +16,14 @@ std::vector<Point> Plane::draw() const {
     auto p3 = Point( half, 0,  half);
 
     //1st triangle
-    coords.push_back(p0);
-    coords.push_back(p2);
-    coords.push_back(p1);
+    coords.emplace_back(p0, v, 0, 0);
+    coords.emplace_back(p2, v, 0, 1);
+    coords.emplace_back(p1, v, 1, 0);
 
     //2nd triangle
-    coords.push_back(p1);
-    coords.push_back(p2);
-    coords.push_back(p3);
+    coords.emplace_back(p1, v, 1, 0);
+    coords.emplace_back(p2, v, 0, 1);
+    coords.emplace_back(p3, v, 1, 1);
 
     return coords;
 }

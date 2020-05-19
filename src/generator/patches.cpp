e7d256fc01b32836651613c1b2ef91ea93473a71
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2020-05-19 15:45:08 +0100

    [generator] add normals and textures to patches

--- src/generator/patches.cpp
+++ src/generator/patches.cpp
@@ -1,7 +1,5 @@
 #include "generator/patches.hpp"
 
-#include "utils/point.hpp"
-
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
@@ -40,7 +38,7 @@ Patches::Patches(int argc, char** argv) {
 
 auto bezier_point(
     float t, Point const p0, Point const p1, Point const p2, Point const p3)
-    -> Point {
+    -> std::pair<Point, Vector> {
     // bezier matrix
     const float m[4][4] = {
         {-1.0f, +3.0f, -3.0f, +1.0f},
@@ -67,10 +65,18 @@ auto bezier_point(
     for (i32 j = 0; j < 3; j++)
         for (i32 k = 0; k < 4; k++) pv[j] += tv[k] * a[k][j];
 
-    return Point(pv[0], pv[1], pv[2]);
+    // dv = tvd X a
+    const float tvd[4] = {3 * t * t, 2 * t, 1, 0};
+    float dv[3] = {0};
+    for (i32 j = 0; j < 3; j++)
+        for (i32 k = 0; k < 4; k++) dv[j] += tvd[k] * a[k][j];
+
+    return {
+        Point(pv[0], pv[1], pv[2]), Vector(dv[0], dv[1], dv[2]).normalize()};
 }
 
-auto Patches::patch_generator(u64 patch, float u, float v) const -> Point {
+auto Patches::patch_generator(u64 patch, float u, float v) const
+    -> std::pair<Point, Vector> {
     std::vector<Point> patch_control_points;
     for (auto const& index : _index_patches[patch]) {
         patch_control_points.push_back(_control_points[index]);
@@ -80,7 +86,8 @@ auto Patches::patch_generator(u64 patch, float u, float v) const -> Point {
     for (auto i = patch_control_points.cbegin();
          i != patch_control_points.cend();
          i += 4) {
-        new_patch_points.push_back(bezier_point(u, i[0], i[1], i[2], i[3]));
+        auto p = bezier_point(u, i[0], i[1], i[2], i[3]);
+        new_patch_points.push_back(std::get<0>(p));
     }
 
     return bezier_point(
@@ -91,30 +98,42 @@ auto Patches::patch_generator(u64 patch, float u, float v) const -> Point {
         new_patch_points[3]);
 }
 
-std::vector<Point> Patches::draw() const {
-    std::vector<Point> coords;
+std::vector<ModelPoint> Patches::draw() const {
+    std::vector<ModelPoint> coords;
     const float delta = 1.0f / _tesselation_level;
 
     for (i64 p = 0; p < _n_patches; p++) {
         for (i64 ui = 0; ui < _tesselation_level; ui++) {
             const float uf = ui * delta;
             const float next_uf = (ui + 1) * delta;
+
+            float curr_t_x = ui / static_cast<float>(_tesselation_level);
+            float next_t_x = (ui + 1) / static_cast<float>(_tesselation_level);
+
             for (i64 vi = 0; vi < _tesselation_level; vi++) {
                 const float vf = vi * delta;
                 const float next_vf = (vi + 1) * delta;
 
-                auto p0 = patch_generator(p, uf, vf);
-                auto p1 = patch_generator(p, uf, next_vf);
-                auto p2 = patch_generator(p, next_uf, vf);
-                auto p3 = patch_generator(p, next_uf, next_vf);
+                float curr_t_y = vi / static_cast<float>(_tesselation_level);
+                float next_t_y =
+                    (vi + 1) / static_cast<float>(_tesselation_level);
 
+                auto p0 =
+                    ModelPoint(patch_generator(p, uf, vf), curr_t_x, curr_t_y);
+                auto p1 = ModelPoint(
+                    patch_generator(p, uf, next_vf), curr_t_x, next_t_y);
+                auto p2 = ModelPoint(
+                    patch_generator(p, next_uf, vf), next_t_x, curr_t_y);
+                auto p3 = ModelPoint(
+                    patch_generator(p, next_uf, next_vf), next_t_x, next_t_y);
+
+                coords.push_back(p1);
+                coords.push_back(p2);
                 coords.push_back(p3);
-                coords.push_back(p2);
-                coords.push_back(p1);
 
-                coords.push_back(p2);
-                coords.push_back(p0);
                 coords.push_back(p1);
+                coords.push_back(p0);
+                coords.push_back(p2);
             }
         }
     }
